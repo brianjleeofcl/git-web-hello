588103ab3d7adc807370ff77c0616770e0599748
Brian Lee <brianjleeofcl@gmail.com>
2017-12-03 18:03:28 -0800

    Add readme & dynamic routes

---
+++ README.md
@@ -0,0 +1,13 @@
+# go-web-hello
+
+Example server, written in go
+
+### routes 
+
+##### `/`
+
+- `GET`: responds with text "Hello World, from server" (I know, not that creative)
+
+##### `/responsive/:name`
+
+- `GET`: responds with text "Hello ${name}, from server"
--- server.go
+++ server.go
@@ -7,10 +7,17 @@ import (
 
 func main() {
 	http.HandleFunc("/", serveData)
+	http.HandleFunc("/responsive/", responsiveData)
 	http.ListenAndServe(":8080", nil)
 }
 
 func serveData(res http.ResponseWriter, req *http.Request) {
 	response := "Hello World, from server"
+	fmt.Fprintln(res, response)
+}
+
+func responsiveData(res http.ResponseWriter, req *http.Request) {
+	name := req.URL.Path[len("/responsive/"):]
+	response := "Hello " + name + ", from server"
 	fmt.Fprintln(res, response)
 }
